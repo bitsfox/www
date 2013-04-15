1c86661a497d2aa531b491e6a408b293d5d4683d
tybitsfox <root@tiny.com>
2013-04-15 10:18:54 +0800

    2013-4-15 修改了my_wm.txt中包含文件的链接

--- myconfig/source.php
+++ myconfig/source.php
@@ -2,13 +2,17 @@
 
 <?php
 echo "<font size=2><pre><a name=so01></a>";
-include_once("./dwmconfig.h");
+include_once("./dwmconfig.h.txt");
+echo "<hr size=2 width=80% color=red>";
 echo "<a name=so02></a>";
 include_once("./fluxbox_keys.txt");
+echo "<hr size=2 width=80% color=red>";
 echo "<a name=so03></a>";
 include_once("./twmrc");
+echo "<hr size=2 width=80% color=red>";
 echo "<a name=so04></a>";
 include_once("./rc.lua");
+echo "<hr size=2 width=80% color=red>";
 echo "<a name=so05></a>";
 include_once("./fluxbox_init.txt");
 echo "</pre>";
