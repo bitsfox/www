18ed3d2c2b838c01affd3b152dd5d34ac99a7a54
tybitsfox <tybitsfox@163.com>
2013-10-23 21:04:57 +0800

    2013-10-23 修改github的链接地址

--- dns_01.php
+++ dns_01.php
@@ -4,7 +4,7 @@ echo "<br><br><a href='http://www.pubyun.com/' target=_blank>希网动态</a><br
 echo "<a href='http://main.meibu.com/ip/' target=_blank>每步域名解析</a><br>";
 echo "<a href='http://115.com' target=_blank>我的115网盘</a><br>";
 echo "<a href='http://pan.baidu.com/netdisk/home' target=_blank>我的百度网盘</a><br>";
-echo "<a href='http://www.github.com' target=_blank>GitHub</a><br>";
+echo "<a href='https://www.github.com' target=_blank>GitHub</a><br>";
 echo "<a href='https://bitbucket.org/' target=_blank>bitbucket</a><br>"
 
 ?>
