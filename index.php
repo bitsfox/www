5d0fb0a3d803d050104f1f426350dcead94a9357
tybitsfox <tybitsfox@163.com>
2014-05-23 22:45:41 +0800

    2014-5-23添加了php的本地中文手册

--- index.php
+++ index.php
@@ -78,6 +78,7 @@ if($_POST[action]==search)
 echo "</td></tr></table></form> </td></tr></table>";
 echo "<a href='./chgmain_idx.php?usersel=old'>返回老版主页</a><br><br>";
 echo "<a href='./www/index.php' target=_blank>进入lims系统</a>";
+echo "<br><br><a href='./php-chunked-xhtml/index.html' target=_blank>php</a>";
 }
 //echo "end of search....<br>";
 ?><!--
