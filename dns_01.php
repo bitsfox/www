e773592ba268b2c5daf3dedc119a7f2e122365c8
tybitsfox <tybitsfox@163.com>
2014-06-03 23:05:57 +0800

    2014-6-3添加了koding的介绍。并上传上一版本的网站至tybitsfox.kd.io

--- dns_01.php
+++ dns_01.php
@@ -6,6 +6,8 @@ echo "<a href='http://115.com' target=_blank>我的115网盘</a><br>";
 echo "<a href='http://pan.baidu.com/netdisk/home' target=_blank>我的百度网盘</a><br>";
 echo "<a href='http://yunpan.360.cn/' target=_blank>我的360网盘</a><br>";
 echo "<a href='https://www.github.com' target=_blank>GitHub</a><br>";
-echo "<a href='https://bitbucket.org/' target=_blank>bitbucket</a><br>"
+echo "<a href='https://bitbucket.org/' target=_blank>bitbucket</a><br>";
+echo "<a href='https://tybitsfox.kd.io/' target=_blank>我的koding主页</a><br>";
+
 
 ?>
