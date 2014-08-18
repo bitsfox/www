f32452b41356b8e095b38848958416a791186aac
tybitsfox <tybitsfox@126.com>
2014-08-18 22:33:51 +0800

    2014-08-18添加了bochs的说明，添加了保护模式重定位的补充说明

--- kernel/boot.php
+++ kernel/boot.php
@@ -5,6 +5,9 @@ include_once("./about_booting.txt");
 echo "</pre>";
 echo "<br><pre>";
 include_once("./bios_int.txt");
+echo "</pre><br><br>";
+echo "<a name=bochs></a><font color=red font=5>bochs的配置</font><pre>";
+include_once("./about_boch.txt");
 echo "</pre>";
 ?>
 
