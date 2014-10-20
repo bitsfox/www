619982c4583aa4d0d4c7bd711d731cc953c922fc
tybitsfox <tybitsfox@163.com>
2014-10-20 11:26:43 +0800

    2014-10-20添加了操作系统开发系列网站的本地资料，添加了软驱驱动的编程介绍

--- kernel/k_main.php
+++ kernel/k_main.php
@@ -26,7 +26,8 @@ echo "<a href=./topic.php target=_blank>linux0.11内核学习杂谈</a><br><br>"
 echo "<font size=4>&nbsp;&nbsp;&nbsp;五、引导及多任务测试:<br><br>";
 echo "<a href=./kernel_task.php target=_blank>引导及多任务源代码</a><br><br>";
 echo "<font size=4>&nbsp;&nbsp;&nbsp;六、驱动编程资料:<br><br>";
-echo "<a href=./oss.org.cn/kernel-book/ldd3/index.html target=_blank>Linux设备驱动第三版</a><br><br>";
+echo "<a href=./oss.org.cn/kernel-book/ldd3/index.html target=_blank>Linux设备驱动第三版</a>&nbsp;&nbsp;&nbsp;&nbsp;
+<a href=/www.brokenthorn.com/Resources/OSDevIndex.html target=_blank>操作系统开发系列</a><br><br>";
 echo "</td></tr></table>";
 
 ?>
