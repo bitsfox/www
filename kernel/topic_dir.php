322f46042af82b694777f647fd36b84aadcf1d2d
tybitsfox <tybitsfox@163.com>
2014-11-21 09:28:59 +0800

    2014-11-21添加了内核编程心得的索引，添加了c与汇编混编重定位的说明

---
+++ kernel/topic_dir.php
@@ -0,0 +1,11 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<?php
+echo "<br><center><font color=red size=4>慢慢积累的东西多了，为了快速查阅，只能再开一个检索页面了</font></center><br><br>";
+echo "<a href=topic.php#prob1>内核模块重定位问题一</a><br><br>";
+echo "<a href=topic.php#prob2>内核重定位问题补充</a><br><br>";
+echo "<a href=topic.php#floppy>关于软驱驱动编程</a><br><br>";
+echo "<a href=topic.php#lcall>远调用及常量定义心得</a><br><br>";
+echo "<a href=topic.php#task>关于任务切换和中断门、陷阱门的心得</a><br><br>";
+echo "<a href=topic.php#c_asm>关于汇编和c混编重定位的问题</a><br><br>";
+
+?>
