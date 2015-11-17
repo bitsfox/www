cb5b3975e4611bb41f129fe18a23adc71eceebf0
tybitsfox <tybitsfox@126.com>
2015-11-17 15:16:22 +0800

    2015-11-17 添加了mm3的地图和mm2r攻略的快速索引

--- tin16/mm3.php
+++ tin16/mm3.php
@@ -2,6 +2,11 @@
 <?php
 echo "<center><font color=red size=6>NDS重装机兵3攻略</font></center><br><pre>";
 include_once("./nds_mm3.txt");
-echo "</pre>";
+echo "</pre><br>";
+echo "
+<table border=0 width=100%><tr width=100%>
+<td width=100% align=center><img src='./mmr_pic/mm31.jpg' /></td><td width=10%></td>
+</tr>
+</table>";
 ?>
 
