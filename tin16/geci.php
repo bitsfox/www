5268091b4954938e27a313a097d638cf51648dee
tybitsfox <tybitsfox@126.com>
2015-12-11 10:19:12 +0800

    2015-12-11 添加了我喜欢的歌曲歌词

---
+++ tin16/geci.php
@@ -0,0 +1,273 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<?php
+echo "<table width=100% border=0><tr width=100%><td width=100% colspan=4>
+<center><font color=red size=5>Without you -Harry Nilsson</font></center>
+</td></tr>
+
+<tr width=100%><td width=10%></td><td width=40% align=left>";
+echo "<pre><font color=blue>
+No I can't forget this evening
+Or your face as you were leaving
+But I guess that's just the way The story goes
+You always smile but in your eyes
+You sorrow shows ,Yes it shows
+No I can't forget tomorrow
+When I think of all my sorrow
+When I had you there ,But then I let you go
+And now it's only fair,That I should let you know
+What you should know
+I can't live, If living is without you
+I can't live, I can't give anymore
+I can't live, If living is without you
+I can't live, I can't give anymore
+Well I can't forget this evening
+Or your face as you were leaving
+But I guess that's just the way The story goes
+You always smile but in your eyes
+You sorrow shows,Yes it shows
+I can't live, If living is without you
+I can't live, I can't give anymore
+I can't live, If living is without you
+I can't live, I can't give anymore
+</font></pre></td><td width=40%><pre>
+我无法忘记今晚
+当你离去时的脸庞
+但我想,那就是故事的结局
+你一直保持着微笑,但你的眼睛
+却掩饰不住悲哀,是的,那是悲哀
+我无法忘记明天
+当所有的哀愁又涌上心头
+我曾经拥有你,而现在我却让你离开
+在这别离的时刻,我应该让你知道
+让你知道你该知道的事
+我无法生存,如果生命中没有你
+我无法生存,我已再不能忍受
+我无法生存,如果生命中没有你
+我无法生存,我已再不能忍受
+我无法忘记今晚
+当你离去时的脸庞
+但我想,那就是故事的结局
+你一直保持着微笑,但你的眼睛
+却掩饰不住悲哀,是的,那是悲哀
+我无法生存,如果生命中没有你
+我无法生存,我已再不能忍受
+我无法生存,如果生命中没有你
+我无法生存,我已再不能忍受
+</pre></td><td width=10%></td></tr>";
+echo "<tr width=100%><td colspan=4 width=100%><center><font color=red size=5>No matter what -Boyzone</font></center></td></tr>";
+echo "<tr><td width=10%></td><td width=30%><pre><font color=blue>
+No matter what they tell us
+No matter what they do
+No matter what they teach us
+What we believe is true
+
+No matter what they call us
+However they attack
+No matter where they take us
+We'll find our own way back
+
+I can't deny what I believe
+I can't be what I'm not
+I know our love forever
+I know, no matter what
+
+If only tears were laughter (ooh)
+If only night was day (ooh)
+If only prayers were answered (hear my prayers)
+Then we would hear God say (say)
+
+No matter what they tell you (ooh)
+No matter what they do (ooh)
+No matter what they teach you
+What you believe is true
+
+And I will keep you safe and strong
+And shelter from the storm
+No matter where it's barren
+A dream is being born
+
+(Ooh) 
+No matter who they follow
+No matter where they lead
+No matter how they judge us
+I'll be everyone you need
+
+No matter if the sun don't shine (sun don't shine)
+Or if the skies are blue (skies are blue)
+No matter what the end is
+My life began with you
+
+I can't deny what I believe (what I believe, yeah)
+I can't be what I'm not
+(I know, I know) I know this love's forever
+That's all that matters now
+No matter what
+
+No matter what (no, no matter, no)
+No, no matter
+That's all that matters to me
+</font></pre></td>
+<td width=30%><pre>
+无论他们如何告诉我们 
+无论他们对我们做什么 
+无论他们教给我们什么 
+我们坚信的才是真理 
+
+无论他们如何称呼我们 
+无论他们如何诋毁我们 
+无论他们把我们带到哪里 
+我们将找到自己回来的路 
+
+我不能背叛我的信念 
+我不能虚伪地活着 
+我知道我们的爱将永恒 
+我知道，无论如何 
+
+一旦眼泪化作微笑 
+一旦黑夜变成白昼 
+一旦祈祷得到回答(听我的祈祷) 
+我们就能听到上帝的声音 
+
+无论他们如何告诉你们 
+无论他们对你们作什么 
+无论他们教给你们什么 
+你们坚信的才是真理 
+
+我将保佑你们平安和强壮 
+也为你们挡风遮雨 
+无论那里如何荒芜 
+一个梦想正在诞生
+
+无论他们追随着谁 
+无论他们去向哪里 
+无论他们如何判决我们 
+我将永远和你在一起 
+
+无论太阳是否明亮(太阳暗了) 
+或者天空是否蔚蓝(天空是蓝色的) 
+无论结局如何 
+我的生命从你开始 
+
+我不能背叛我的信念(我的信念) 
+我不能虚伪地活着 
+(我知道，我知道)我知道这爱将永恒 
+现在这就是一切 
+无论如何 
+
+无论如何(无论) 
+无论 
+对我来说，这就是一切
+</pre></td>
+<td width=10%></td></tr>";
+echo "<tr width=100%><td colspan=4 width=100%><center><font color=red size=5>Edelweiss</font></center></td></tr>";
+echo "<tr><td width=10%></td><td width=30%><pre><font color=blue>
+edelweiss, edelweiss
+every morning you greet me
+small and white, clean and bright
+you look happy to meet me
+blossom of snow may you bloom and grow
+bloom and grow forever
+edelweiss, edelweiss
+bless my homeland forever
+small and white, clean and bright
+you look happy to meet me
+blossom of snow may you bloom and grow
+bloom and grow forever
+edelweiss, edelweiss
+bless my homeland forever
+</font></pre></td>
+<td width=30%><pre>
+雪绒花,雪绒花
+每天早晨你都欢迎著我
+小而白,洁又亮
+你看起来好像很高兴遇见我
+雪白的花朵,你盛开茁壮
+永远开放、成长
+雪绒花,雪绒花
+永远保佑我的国家
+小而白,洁又亮
+你看起来好像很高兴遇见我
+雪白的花朵,你盛开茁壮
+永远开放、成长
+雪绒花,雪绒花
+永远保佑我的国家
+</pre></td>
+<td width=10%></td></tr>";
+
+echo "<tr width=100%><td colspan=4 width=100%><center><font color=red size=5>Jingle Bells</font></center></td></tr>";
+echo "<tr><td width=10%></td><td width=30%><pre><font color=blue>
+Dashing through the snow
+On a one-horse open sleigh
+Over the fields we go
+Laughing all the way
+Bells on bob-tail ring
+making spirits bright
+What fun it is to ride and sing
+A sleighing song tonight
+
+Jingle bells, Jingle Bells
+jingle all the way
+Oh what fun it is to ride
+In a one-horse open sleigh
+Jingle Bells, Jingle Bells
+Jingle all the way
+Oh what fun it is to ride
+In a one horse open sleigh
+
+A day or two ago
+I thought I’d take a ride
+And soon Miss Fanny Bright
+Was seated by my side
+The horse was lean and lank
+Misfortune seemed his lot
+He got into a drifted bank
+And we, we got upsot
+Jingle Bells, Jingle Bells
+Jingle all the way
+Oh What fun it is to ride
+In a one-horse open sleigh
+Jingle Bells, Jingle Bells
+Jingle all the way
+Oh what fun it is to ride
+In a one horse open sleigh
+</font></pre></td>
+<td width=30%><pre>
+啊冲破大风雪，
+我们坐在雪橇上，
+快奔驰过田野，
+我们欢笑又歌唱，
+马儿铃声响叮当，
+令人精神多欢畅，
+我们今晚滑雪真快乐，
+把滑雪歌儿唱。
+
+叮叮当，叮叮当，
+铃儿响叮当，
+今晚滑雪多快乐，
+我们坐在雪橇上，
+叮叮当，叮叮当，
+铃儿响叮当，
+今晚滑雪多快乐，
+我们坐在雪橇上。
+
+在一两天之前，
+我想出外去游荡，
+那位美丽小姑娘，
+她坐在我身旁，
+那马儿瘦又老，
+它命运不吉祥，
+把雪橇拖进泥塘里，
+害得我们糟了殃。
+叮叮当，叮叮当，
+铃儿响叮当，
+今晚滑雪多快乐，
+我们坐在雪橇上，
+叮叮当，叮叮当，
+铃儿响叮当，
+今晚滑雪多快乐，
+我们坐在雪橇上。
+</pre></td>
+<td width=10%></td></tr>";
+
+echo "</table>";
+?>
