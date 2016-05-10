5e2f86114c46f9ccdc2aa2d7e6cbc30046581735
tybitsfox <tybitsfox@163.com>
2016-05-10 22:08:58 +0800

    2016-5-10添加了关于mysql时间戳转换的示例

--- linux_about/lab_b.php
+++ linux_about/lab_b.php
@@ -1,5 +1,10 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <?php
 include_once("./about_ta_server.txt");
+echo "<font color=blue>mysql中关于时间戳转换的使用：<br>
+select count(*) from fq_d_master where date = from_unixtime(1218290027);<br>
+select count(*) from fq_d_master where unix_timestamp(date) = 121890027;<br>
+<br><br><br><br><br><br><br>
+</blue>";
 ?>
 
