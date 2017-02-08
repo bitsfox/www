c82d242c4b487fa746fd0bbe8d6d4401e6e3f8f6
bitsfox <tybitsfox@126.com>
2017-02-08 15:22:44 +0800

    2017-2-8添加gtk2.24.31版本的资料

--- gtkabout.php
+++ gtkabout.php
@@ -2,7 +2,7 @@
 <?php
 echo "<br><br><center><a href=./www.gtk.org/api/2.6/gtk/index.html target=_blank>GTK API</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=./www.gtk.org/api/2.6/gobject/index.html target=_blank>gobject</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=./www.gtk.org/api/2.6/gdk-pixbuf/index.html target=_blank>gdk-pixbuf</a><br><br>
 <a href=./www.gtk.org/api/2.6/gdk/index.html target=_blank>GDK</a>&nbsp;&nbsp;&nbsp;&nbsp;
-<a href=./www.gtk.org/api/2.6/girdoc/C/index.html target=_blank>girdoc</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=./gtk_about/gtk_simple.php target=_blank>gtk示例</a>
+<a href=./www.gtk.org/api/2.6/girdoc/C/index.html target=_blank>girdoc</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=./gtk_about/gtk_simple.php target=_blank>gtk示例</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=./gtk2-html-2.24.31/index.html target=_blank>gtk2.24.31</a>
 </font></center>";
 echo "<hr><center>2017年更新新版资料<br><br>";
 echo "<a href='./developer.gnome.org/glib/stable/index.html' target=_blank>glib</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./developer.gnome.org/gobject/stable/index.html' target=_blank>gobject</a><br><br>";
