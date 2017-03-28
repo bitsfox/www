105d34451ccf19553c0ad88e585c2f570d8e09de
bitsfox <tybitsfox@126.com>
2017-03-28 09:39:53 +0800

    2017-3-28 添加城通网盘链接

--- dns_01.php
+++ dns_01.php
@@ -1,7 +1,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <?php
 echo "<br><br><a href='http://www.pubyun.com/' target=_blank>希网动态</a><br>";
-echo "<a href='http://main.meibu.com/ip/' target=_blank>每步域名解析</a><br>";
+echo "<a href='https://www.ctfile.com/' target=_blank>城通网盘</a><br>";
 echo "<a href='http://115.com' target=_blank>我的115网盘</a><br>";
 echo "<a href='http://pan.baidu.com/netdisk/home' target=_blank>我的百度网盘</a><br>";
 echo "<a href='http://yunpan.360.cn/' target=_blank>我的360网盘</a><br>";
