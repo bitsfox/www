f9a2d1630e73bff2ffbc87f8ea4eb16661de243c
tybitsfox <tybitsfox@163.com>
2017-05-10 15:28:20 +0800

    2017-5-10完善龙战士3攻略

---
+++ tin16/bbb/bof3.php
@@ -0,0 +1,12 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<?php
+echo "<a name=begin></a><center><font color=blue size=6>龙战士III资料站</font></center>";
+echo "<center><a href=#gonglue>一、游戏攻略</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=#baishi>二、师匠资料</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=#yinzi>三、龙因子资料</a></center>";
+echo "<br><br><a name=gonglue></a>";
+include_once("./bof3gl.php");
+echo "<center><a href=#begin>返  回</a><br><br><a name=baishi></a>";
+include_once("./baishi.php");
+echo "<center><a href=#begin>返  回</a></center><br><br><a name=yinzi></a>";
+include_once("./yinzi.php");
+echo "<center><a href=#begin>返  回</a>";
+?>
