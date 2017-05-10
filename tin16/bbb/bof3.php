4cbba7405141ec445a74c45b59aa55f2de6a203e
tybitsfox <tybitsfox@163.com>
2017-05-11 00:14:06 +0800

    2016-5-10完善龙战士3攻略

--- tin16/bbb/bof3.php
+++ tin16/bbb/bof3.php
@@ -1,7 +1,9 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <?php
-echo "<a name=begin></a><center><font color=blue size=6>龙战士III资料站</font></center>";
-echo "<center><a href=#gonglue>一、游戏攻略</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=#baishi>二、师匠资料</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=#yinzi>三、龙因子资料</a></center>";
+echo "<a name=begin></a><center><font color=blue size=6>龙战士III资料站</font></center><br>";
+echo "<center><a href=#gonglue>一、游戏攻略</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=#baishi>二、师匠资料</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href=#yinzi>三、龙因子资料</a>
+&nbsp;&nbsp;&nbsp;&nbsp;<a href='./xinde.php#xinde'>四、拜师心得</a>
+</center>";
 echo "<br><br><a name=gonglue></a>";
 include_once("./bof3gl.php");
 echo "<center><a href=#begin>返  回</a><br><br><a name=baishi></a>";
