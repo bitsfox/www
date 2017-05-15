dfe047dad35807aa135884b6af839ddc04fda810
bitsfox <tybitsfox@126.com>
2017-05-15 17:14:06 +0800

    2017-5-15继续完善龙战士4攻略

--- tin16/bbb/bof4.php
+++ tin16/bbb/bof4.php
@@ -4,6 +4,6 @@ echo "<a name=begin></a><center><font color=red size=6>龙战士4资料</font><b
 echo "<a href='./bof4gonglue.php#begin'>第一章：觉醒的人</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./bof4gonglue.php#sec_002'>第二章：不变的人</a>&nbsp;&nbsp;&nbsp;&nbsp;
 <a href='./bof4gonglue.php#sec_003'>第三章：遗忘</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./bof4gonglue.php#sec_004'>终章：变化的人</a>&nbsp;&nbsp;&nbsp;&nbsp;<hr width=90% border=1>
 <a href='./bof4gonglue.php#sec_005'>钓鱼地点</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./bof4gonglue.php#sec_006'>习师系统</a>&nbsp;&nbsp;&nbsp;&nbsp;
-<a href='./bof4gonglue.php#sec_007'>合成魔法要点</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./bof4gonglue.php#sec_008'>精灵共同体</a>&nbsp;&nbsp;&nbsp;&nbsp;<br>
-<a href='./bof4quest.php#begin'>问题集绵</a></center>";
+<a href='./bof4gonglue.php#sec_007'>合成魔法要点</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./bof4gonglue.php#sec_008'>精灵共同体</a>&nbsp;&nbsp;&nbsp;&nbsp;<br><br>
+<a href='./bof41.php#begin'>问题集绵</a></center>";
 ?>
