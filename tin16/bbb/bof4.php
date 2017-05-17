20bbadf746fa081a49878300c1352f54644e7619
bitsfox <tybitsfox@126.com>
2017-05-17 16:32:24 +0800

    2017-5-17完善龙战士4的攻略

--- tin16/bbb/bof4.php
+++ tin16/bbb/bof4.php
@@ -5,7 +5,8 @@ echo "<a href='./bof4gonglue.php#begin'>第一章：觉醒的人</a>&nbsp;&nbsp;
 <a href='./bof4gonglue.php#sec_003'>第三章：遗忘</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./bof4gonglue.php#sec_004'>终章：变化的人</a>&nbsp;&nbsp;&nbsp;&nbsp;<hr width=90% border=1>
 <a href='./bof4gonglue.php#sec_005'>钓鱼地点</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./bof4gonglue.php#sec_006'>习师系统</a>&nbsp;&nbsp;&nbsp;&nbsp;
 <a href='./bof4gonglue.php#sec_007'>合成魔法要点</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./bof4gonglue.php#sec_008'>精灵共同体</a>&nbsp;&nbsp;&nbsp;&nbsp;<br><br>
-<a href='./bof41.php#begin'>问题集绵</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./bof4.php#pic_001'>龙四大地图</a></center><br><br><br><br><br><br><br><br><br><br><br>";
+<a href='./bof41.php#begin'>问题集绵</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./bof4.php#pic_001'>龙四大地图</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./b44.php' target=_blank>技能</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href='./b45.php#begin'>人物养成</a>
+</center><br><br><br><br><br><br><br><br><br><br><br>";
 echo "<a name=pic_001></a>
 <table border=0 width=100%><tr width=100%><td width=10%></td>
 <td width=80% align=center><img src='./bof3_pic/bof4.jpeg' style='display:block;width:70%;' /></td><td width=10%></td>
