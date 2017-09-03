a02f5e022434c8a3ec8567dba40d9ea3e715fd15
tybitsfox <tyyyyt@163.com>
2017-09-03 12:52:10 +0800

    2017-9-3删除之前系统遗留的代码

--- googlemap/gis_hb/config/main.php
+++ googlemap/gis_hb/config/main.php
@@ -47,15 +47,9 @@ require_once($str11);
 	$menuitem[0]			=	"点位总览";
 	$menuitem[1]			=	"点位详细";
 	$menuitem[2]			=	"监测数据";
-//	$menuitem[3]			=	"废水明晰";
-//	$menuitem[4]			=	"废气明晰";
-//	$menuitem[5]			=	"污水厂明晰";
 	$menulnk[0]				=	"index.php?pgcnt=".base64_encode("template/01/m01.html");
 	$menulnk[1]				=	"index.php?pgcnt=".base64_encode("template/01/m02.html");
 	$menulnk[2]				=	"index.php?pgcnt=".base64_encode("template/01/m03.html");
-//	$menulnk[3]				=	"index.php?pgcnt=".base64_encode("template/01/m04.html");
-//	$menulnk[4]				=	"index.php?pgcnt=".base64_encode("template/01/m05.html");
-//	$menulnk[5]				=	"index.php?pgcnt=".base64_encode("template/01/m06.html");
 //
 	$arry					=	array();
 //下面的数组用于绘制图形	
@@ -68,66 +62,8 @@ require_once($str11);
 	$yway_cnt				=	6;		//纵坐标的刻度个数
 
 /////////////////下面添加的用于按年度访问不同数据库服务器上的数据
-//	$DB_ADDR_TY				=	array('2016'=>'127.0.0.1','2017'=>'127.0.0.1');
-//	$DB_PORT_TY				=	array('2016'=>3306,'2017'=>3306);
-//	$DB_NAME_TY				=	array('2016'=>'env2016','2017'=>'env2017');
-//include_once("./config/setup.php");
 	$DB_USER_TY				=	"sdhl";
 	$DB_PWD_TY				=	"sdhl2016";
-/////////////////废水和污水厂////////////////////////////////////////////
-	$FS_HEADER				=	"<table width=97%% class='imagetable'><tr><th width=10%% rowspan=2 align=center>编号</th>
-								 <th width=20%% rowspan=2 align=center>时间</th><th width=20%% colspan=2 align=center>COD</th>
-								 <th width=20%% colspan=2 align=center>氨氮</th><th width=30%% colspan=3 align=center>流量</th></tr><tr>
-								 <th width=10%% align=center>监测值</th><th width=10%% align=center>标准值</th>
-								 <th width=10%% align=center>监测值</th><th width=10%% align=center>标准值</th>
-								 <th width=10%% align=center>瞬时流量</th><th width=10%% align=center>间隔流量</th>
-								 <th width=10%% align=center>累计流量</th></tr>";
-	$FS_BODY_1				=	"<tr><td width=10%% >%d</td><td width=20%% >%s</td>";
-	$FS_BODY_RED			=	"<td width=10%% id='tdid'>%0.2f</td><td width=10%% >%0.2f</td>";
-	$FS_BODY_NOR			=	"<td width=10%% >%0.2f</td><td width=10%% >%0.2f</td>";
-	$FS_BODY_LL				=	"<td width=10%% >%u</td><td width=10%% >%u</td><td width=10%% >%u</td></tr>";	
-	$FS_HEADER_END			=	"</table>";
-////////////////////废气//////////////////////////////////////
-	$FQ_HEADER				=	"<table width=97%% class='imagetable'><tr><th width=6%% rowspan=2 align=center>编号</th>
-								<th width=20%% rowspan=2 align=center>时间</th><th width=16%% colspan=2 align=center>SO2</th>
-								<th width=16%% colspan=2 align=center>NOx</th><th width=16%% colspan=2 align=center>烟尘</th>
-								<th width=10%% rowspan=2 align=center>氧量</th><th width=16%% rowspan=2 align=center>废气排放量</th></tr><tr>
-								<th width=8%% align=center>监测值</th><th width=8%% align=center>标准值</th>
-								<th width=8%% align=center>监测值</th><th width=8%% align=center>标准值</th>
-								<th width=8%% align=center>监测值</th><th width=8%% align=center>标准值</th></tr>";
-	$FQ_BODY_1				=	"<tr><td width=5%% >%d</td><td width=20%% >%s</td>";
-	$FQ_BODY_RED			=	"<td width=8%% id='tdid'>%0.2f</td><td width=8%% >%0.2f</td>";
-	$FQ_BODY_NOR			=	"<td width=8%% >%0.2f</td><td width=8%% >%0.2f</td>";
-	$FQ_BODY_LL				=	"<td width=10%% >%0.2f</td><td width=16%% >%u</td></tr>";
-	$FQ_HEADER_END			=	"</table>";
-////////////////////废水和污水厂实时/////////////////////////////////////////////
-	$FSS_HEADER				=	"<table width=97%% class='imagetable'><tr><th width=5%% rowspan=2 align=center>编号</th>
-								 <th width=15%% rowspan=2 align=center>站点名称</th><th width=10%% rowspan=2 align=center>上传时间</th>
-								 <th width=20%% colspan=2 align=center>COD</th><th width=20%% colspan=2 align=center>氨氮</th>
-								 <th width=30%% colspan=3 align=center>流量</th></tr><tr>
-								 <th width=10%% align=center>监测值</th><th width=10%% align=center>标准值</th>
-								 <th width=10%% align=center>监测值</th><th width=10%% align=center>标准值</th>
-								 <th width=10%% align=center>瞬时流量</th><th width=10%% align=center>间隔流量</th>
-								 <th width=10%% align=center>累计流量</th></tr>";
-	$FSS_BODY_1				=	"<tr><td width=5%% >%d</td><td width=15%% >%s</td><td width=10%% >%s</td>";
-	$FSS_BODY_RED			=	"<td width=10%% id='tdid'>%s</td><td width=10%% >%s</td>";
-	$FSS_BODY_NOR			=	"<td width=10%% >%s</td><td width=10%% >%s</td>";
-	$FSS_BODY_LL			=	"<td width=10%% >%s</td><td width=10%% >%s</td><td width=10%% >%s</td></tr>";	
-	$FSS_HEADER_END			=	"</table>";
-/////////////////////废气实时/////////////////////////////////////////////////////////
-	$FQS_HEADER				=	"<table width=97%% class='imagetable'><tr><th width=5%% rowspan=2 align=center>编号</th>
-								<th width=19%% rowspan=2 align=center>站点名称</th><th width=10%% rowspan=2 align=center>上传时间</th>
-								<th width=16%% colspan=2 align=center>SO2</th>
-								<th width=16%% colspan=2 align=center>NOx</th><th width=16%% colspan=2 align=center>烟尘</th>
-								<th width=8%% rowspan=2 align=center>氧量</th><th width=10%% rowspan=2 align=center>废气排放量</th></tr><tr>
-								<th width=8%% align=center>监测值</th><th width=8%% align=center>标准值</th>
-								<th width=8%% align=center>监测值</th><th width=8%% align=center>标准值</th>
-								<th width=8%% align=center>监测值</th><th width=8%% align=center>标准值</th></tr>";
-	$FQS_BODY_1				=	"<tr><td width=5%% >%d</td><td width=19%% >%s</td><td width=10%% >%s</td>";
-	$FQS_BODY_RED			=	"<td width=8%% id='tdid'>%s</td><td width=8%% >%s</td>";
-	$FQS_BODY_NOR			=	"<td width=8%% >%s</td><td width=8%% >%s</td>";
-	$FQS_BODY_LL			=	"<td width=8%% >%s</td><td width=10%% >%s</td></tr>";
-	$FQS_HEADER_END			=	"</table>";
 //////////////////////////////2016-12-19添加无效值的定义/////////////////////////////
 	define("IGN_VAL",-100.11);	
 	$WUXIAO					=	"无效值";
