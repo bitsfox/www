f02672f71ab25d2f0e0220975126fe7cbd50fc9e
tybitsfox <tyyyyt@163.com>
2017-09-24 09:52:39 +0800

    2017-9-24添加版权信息

--- googlemap/gis_hb/interface/main.php
+++ googlemap/gis_hb/interface/main.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * @package base
+ * @version 0.1.0.0
+ * @author 田勇 Alias tybitsfox <tybitsfox@163.com>
+ * @copyright (c) 2017 by tybitsfox
+ * @license GPLv2
+ * 
+ * 本文件是土壤环境信息系统其中的一个文件
+ * Available at http://202.102.134.68
+ *
+ * 这一程序是自由软件，你可以遵照自由软件基金会出版的GNU通用公共许可证条款来修改和重新
+ * 发布这一程序。或者用许可证的第二版，或者（根据你的选择）用任何更新的版本。
+ *
+ * 发布这一程序的目的是希望它有用，但没有任何担保。甚至没有适合特定目的的隐含的担保。更
+ * 详细的情况请参阅GNU通用公共许可证。
+ *
+ * 你应该已经和程序一起收到一份GNU通用公共许可证的副本。如果还没有，请查阅：
+ * <http://www.gnu.org/licenses/>.
+ * 或者写信给： The Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA  02111-1307  USA
+ */
+?>
+<?php
 /*这是一个由接口导出的类实现文件
 本文件实现下列功能：
 1、独立的验证和注册函数
