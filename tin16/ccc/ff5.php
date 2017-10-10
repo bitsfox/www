15d5c991a023e7dbdac284f3c5d4416d503d21a3
tybitsfox <tyyyyt@163.com>
2017-10-10 17:13:25 +0800

    2017-10-10 update ff5 again....

--- tin16/ccc/ff5.php
+++ tin16/ccc/ff5.php
@@ -11,31 +11,31 @@ echo "<p><strong>(1) 攻略總目錄</strong></p>
 <td >第一世界</td><td >支線跳轉</td><td>鋼琴/技能</td>
 </tr>
 <tr >
-<td ><a href='ff5_part1_1.php' target='_blank'>1-1 風之水晶</a></td><td><a href='ff5_part1_1.php/#初心者之館' target='_blank'>初心者之館</a></td><td>琴：<span style='color: #800000;'>圖爾村</span></td>
+<td ><a href='ff5_part1_1.php' target='_blank'>1-1 風之水晶</a></td><td><a href='ff5_part1_1.php#初心者之館' target='_blank'>初心者之館</a></td><td>琴：<span style='color: #800000;'>圖爾村</span></td>
 </tr>
 <tr >
-<td ><a href='ff5_part1_2.php' target='_blank'>1-2 水之水晶</a></td><td><a href='ff5_part1_2.php/#泰空城' target='_blank'>泰空城</a></td><td >琴：<span style='color: #800000;'>卡文鎮</span></td>
+<td ><a href='ff5_part1_2.php' target='_blank'>1-2 水之水晶</a></td><td><a href='ff5_part1_2.php#泰空城' target='_blank'>泰空城</a></td><td >琴：<span style='color: #800000;'>卡文鎮</span></td>
 </tr>
 <tr >
 <td ><a href='ff5_part1_3.php' target='_blank'>1-3 火之水晶</a></td><td ></td><td >琴：<span style='color: #800000;'>卡納克鎮</span></td>
 </tr>
 <tr >
-<td ><a href='ff5_part1_4.php' target='_blank'>1-4 土之水晶 (上)</a></td><td ><a href='ff5_part1_4.php/#沃爾斯城' target='_blank'>小妖魔</a><br/>
-<a href='ff5_part1_4.php/#新月鎮' target='_blank'>新月鎮</a><br/>
-<a href='ff5_part1_4.php/#裡克斯村' target='_blank'>裡克斯村</a><br/>
-<a href='ff5_part1_4.php/#嘉科爾洞窟' target='_blank'>嘉科爾洞窟</a><br/>
-<a href='ff5_part1_4.php/#伊斯特利村' target='_blank'>伊斯特利村</a></td><td >琴：<span style='color: #800000;'>新月鎮</span><br/>
+<td ><a href='ff5_part1_4.php' target='_blank'>1-4 土之水晶 (上)</a></td><td ><a href='ff5_part1_4.php#沃爾斯城' target='_blank'>小妖魔</a><br/>
+<a href='ff5_part1_4.php#新月鎮' target='_blank'>新月鎮</a><br/>
+<a href='ff5_part1_4.php#裡克斯村' target='_blank'>裡克斯村</a><br/>
+<a href='ff5_part1_4.php#嘉科爾洞窟' target='_blank'>嘉科爾洞窟</a><br/>
+<a href='ff5_part1_4.php#伊斯特利村' target='_blank'>伊斯特利村</a></td><td >琴：<span style='color: #800000;'>新月鎮</span><br/>
 琴：<span style='color: #800000;'>嘉科爾鎮</span><br/>
 歌：<span style='color: #800080;'>體力之歌</span><br/>
 歌：<span style='color: #800080;'>誘惑之歌</span><br/>
 黑：<span style='color: #800080;'>蛙變</span><br/>
 歌：<span style='color: #800080;'>愛之歌</span><br/>
-召：<a href='ff5_part1_4.php/#古代圖書館 地圖2' target='_blank'>伊弗利特</a><br/>
-召：<a href='ff5_part1_4.php/#沃爾斯城' target='_blank'>濕婆</a><br/>
-召：<a href='ff5_part1_4.php/#伊斯特利村' target='_blank'>拉姆</a></td>
+召：<a href='ff5_part1_4.php#古代圖書館 地圖2' target='_blank'>伊弗利特</a><br/>
+召：<a href='ff5_part1_4.php#沃爾斯城' target='_blank'>濕婆</a><br/>
+召：<a href='ff5_part1_4.php#伊斯特利村' target='_blank'>拉姆</a></td>
 </tr>
 <tr >
-<td ><a href='ff5_part5.php' target='_blank'>1-5 土之水晶 (下)</a></td><td ></td><td >召：<a href='ff5_part5.php/#卡納克隕石地' target='_blank'>泰坦</a></td>
+<td ><a href='ff5_part1_5.php' target='_blank'>1-5 土之水晶 (下)</a></td><td ></td><td >召：<a href='ff5_part1_5.php#卡納克隕石地' target='_blank'>泰坦</a></td>
 </tr>
 <tr >
 <td >第二世界</td><td >支線跳轉</td><td >鋼琴/技能</td>
