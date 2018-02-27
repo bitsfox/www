3e03bdc30dc661b0339fed317dada552c882c71f
tybitsfox <tyyyyt@163.com>
2018-02-27 20:14:28 +0800

    2018-2-27 add dq5

---
+++ tin16/mmm/dq5.php
@@ -0,0 +1,17 @@
+<?php
+echo "<html><head>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+<link href='./main.css' rel='stylesheet' type='text/css' />";
+echo "<style type='text/css'>
+body{ text-align:center} 
+#divcenter{margin:0 auto;width:800px}</style>
+</head><body>";
+echo "<br><center><font size=6 color=red>勇者斗恶龙5攻略</font></center><br><br>";
+echo "<table align=center width=70% border=1 cellspacing=0 cellpadding=1><tr>";
+echo "<td align=center><a href='./walk.php' target=_blank><font size=4>攻略流程</font></a></td>";
+echo "<td align=center><a href='./.php' target=_blank><font size=4>待添加</font></a></td>";
+echo "<td align=center><a href='./.php' target=_blank><font size=4>待添加</font></a></td>";
+echo "<td align=center><a href='./.php' target=_blank><font size=4>待添加</font></a></td>";
+echo "</tr></table>";
+echo "</body></html>";
+?>
