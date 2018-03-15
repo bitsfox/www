d8b21daa90d5f426583ffaccaa9ad3e4e5e3fc36
tybitsfox <tyyyyt@163.com>
2018-03-15 09:42:44 +0800

    2018-3-15修改计数函数，添加dq5攻略

--- counter.php
+++ counter.php
@@ -10,6 +10,12 @@ function upcounter($sname)
 	mysqli_set_charset($conn,"utf8");
 	$sqlstr="update link_cnt set count=count+1  where link = '".$sname."'";
 	$result=mysqli_query($conn,$sqlstr);
-	mysqli_close($conn);//当前网页计数完毕，开始转向打开网页
+	if(mysqli_affected_rows() == 0)
+	{
+		$fn=substr(strrchr($sname,'/'),1);
+		$sqlstr="insert into link_cnt(link,count,tip) values('".$sname."',0,'".$fn."')";
+		mysqli_query($conn,$sqlstr);
+	}
+	mysqli_close($conn);
 }
 ?>
