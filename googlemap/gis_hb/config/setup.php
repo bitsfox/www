6561ff82c8795475f97b90758ca947edf1d5d117
tybitsfox <tyyyyt@163.com>
2018-04-24 16:39:47 +0800

    2018-4-24修改土壤监测平台的配置文件

--- googlemap/gis_hb/config/setup.php
+++ googlemap/gis_hb/config/setup.php
@@ -27,5 +27,5 @@
 //	$DB_NAME_TY				=	array('2016'=>'gis_hb','2017'=>'gis_hb');
 	$DB_ADDR_TY				=	array('2017'=>'127.0.0.1','2018'=>'127.0.0.1');
 	$DB_PORT_TY				=	array('2017'=>3306,'2018'=>3306);
-	$DB_NAME_TY				=	array('2017'=>'gis_hb','2018'=>'gis_hb');
+	$DB_NAME_TY				=	array('2017'=>'gis_hb','2018'=>'gis_hb2018');
 ?>
