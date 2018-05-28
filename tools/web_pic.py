e96be0cfe54db5f7d6ebbf4be18082b1c41c0069
tybitsfox <tyyyyt@163.com>
2018-05-29 00:11:16 +0800

    2018-5-29 更新数据库备份及图片资料的备份脚本文件

--- tools/web_pic.py
+++ tools/web_pic.py
@@ -16,7 +16,9 @@ if((len(sys.argv) != 2) or (str.upper() == "HELP")):
 	print "\t\t\033[1;31mAuthor:tybitsfox\033[0m 2017-10-23"
 	exit(0)
 base_dir="/var/www/"
-s_dir=("tin16/ggg/ff4_images/","tin16/ccc/images/","tin16/ddd/ff6_images/","tin16/eee/ff2_images","tin16/fff/ff3_images","googlemap/gis_hb/images/","t01/","tin16/ddd/ff6/ff6image/","tin16/hhh/pic/");
+s_dir=("tin16/ggg/ff4_images/","tin16/ccc/images/","tin16/ddd/ff6_images/","tin16/eee/ff2_images","tin16/fff/ff3_images","googlemap/gis_hb/images/",\
+		"tin16/hhh/pic/","tin16/iii/pic1/","tin16/iii/gonglve/","tin16/jjj/walkm2/","tin16/jjj/pic/","tin16/jjj/images/","tin16/kkk/images/","tin16/kkk/tuwen/",\
+		"tin16/lll/120810/","tin16/lll/121214/","tin16/mmm/allimg/");
 #base_dir="./root/documents/aaa/gallery/"
 #s_dir=("ff4_part_1-1","ff4_part_1-2","ff4_part_1-3")
 bakfile="/tmp/web_pic.tar.bz2"
